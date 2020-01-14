c9aad49aa8e7f2f277da41f466135299dc853be4
Jasmine Wang <jasminey.tea@gmail.com>
2020-01-13 21:50:47 -0600

    thinking about some player actions

--- server/store/player.js
+++ server/store/player.js
@@ -1,13 +1,33 @@
 // ACTION TYPES
+const ADD_PLAYER = 'ADD_PLAYER'
+const UPDATE_PLAYER = 'UPDATE_PLAYER'
 
 // ACTION CREATORS
+const addPlayer = player => ({
+  type: ADD_PLAYER,
+  player
+})
 
+const updatePlayer = (id, score) => ({
+  type: UPDATE_PLAYER,
+  id,
+  score
+})
 // INITIAL STATE
-const initialState = [] // {socketId: 0, name: "", score: 0, achievements: ""}
+const initialState = {} // {uuid: { socketId: 0, score: 0, achievements: ""}, uuid: {}, ... }
 
 // REDUCER
 const player = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PLAYER:
+      return [...state, player]
+    case UPDATE_PLAYER:
+      return state.map(el => {
+        if (el.id === action.id) {
+          el.score = action.score
+        }
+        return el
+      })
     default:
       return state
   }
