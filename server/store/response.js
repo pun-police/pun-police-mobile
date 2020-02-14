dba68f5a694bc879a6f2e5381c5ab4bd0140b6fc
Ian Roberts <irob466@gmail.com>
2020-02-13 23:18:26 -0600

    responses have votes now

--- server/store/response.js
+++ server/store/response.js
@@ -2,13 +2,20 @@
 const ADD_RESPONSE = 'ADD_RESPONSE'
 // const CLEAR_ALL = "CLEAR_ALL"
 const LIKE = 'LIKE'
+const VOTE = 'VOTE'
 
 // ACTION CREATORS
 const addResponse = response => ({type: ADD_RESPONSE, response})
 const like = (userId, round) => ({type: LIKE, userId, round})
+const vote = (voteGiverId, voteReceiverId, round) => ({
+  type: VOTE,
+  voteGiverId,
+  voteReceiverId,
+  round
+})
 
 // INITIAL STATE
-const initialState = [] // {userId: UUID, response: "" , likes: 0, round: 0, promptId: 0}
+const initialState = [] // {userId: UUID, response: "" , votes: [userId], likes: 0, round: 0, promptId: 0}
 
 // REDUCER
 const response = (state = initialState, action) => {
@@ -22,6 +29,18 @@ const response = (state = initialState, action) => {
       )
         return [...state, action.response]
       else return state
+    case VOTE:
+      if (
+        state
+          .filter(({round}) => round === action.round)
+          .some(({votes}) => votes.includes(action.voteGiverId))
+      )
+        return state
+      return state.map(s =>
+        s.userId === action.voteReceiverId && s.round === action.round
+          ? {...s, votes: [...s.votes, action.voteGiverId]}
+          : s
+      )
     case LIKE:
       return state.map(s =>
         s.userId === action.userId && s.round === action.round
