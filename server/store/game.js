91fb641c1b4384a0ecd756ce67434b6b227213db
Ian Roberts <irob466@gmail.com>
2020-02-13 21:08:02 -0600

    game reducer now has prompt info

--- server/store/game.js
+++ server/store/game.js
@@ -1,16 +1,20 @@
 // ACTION TYPES
 const CHANGE_PHASE = 'CHANGE_PHASE'
+const CHANGE_PROMPT = 'CHANGE_PROMPT'
 
 // ACTION CREATORS
-const changePhase = (phase, round = 0) => ({
+export const changePhase = (phase, round = 0) => ({
   type: CHANGE_PHASE,
   phase,
   round
 })
 
+export const changePrompt = prompt => ({type: CHANGE_PROMPT, prompt})
+
 // INITIAL STATE
 const initialState = {
   state: 'lobby', // lobby, intro, << prompt, respond, vote, results >>, finish
+  prompt: {}, // {id, text}
   round: 0 // max 3 (number of rounds)
 }
 
@@ -19,6 +23,8 @@ const game = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_PHASE:
       return {phase: action.phase, round: action.round}
+    case CHANGE_PROMPT:
+      return {...state, prompt: action.prompt}
     default:
       return state
   }
