4017fe8883104771207573a651a07ec0f2f9cf6a
Ian Roberts <irob466@gmail.com>
2020-02-13 20:01:16 -0600

    added actions types and creators for responses

--- server/store/response.js
+++ server/store/response.js
@@ -1,13 +1,24 @@
 // ACTION TYPES
+const ADD_RESPONSE = 'ADD_RESPONSE'
+// const CLEAR_ALL = "CLEAR_ALL"
+const LIKE = 'LIKE'
 
 // ACTION CREATORS
+const addResponse = response => ({type: ADD_RESPONSE, response})
+const like = (userId, round) => ({type: LIKE, userId, likedUserId, round})
 
 // INITIAL STATE
-const initialState = [] // {user: "", response: "" , likes: 0, promptId: 0}
+const initialState = [] // {userId: UUID, response: "" , likes: 0, round: 0, promptId: 0}
 
 // REDUCER
 const response = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_RESPONSE:
+      return [...state, action.response]
+    case LIKE:
+      return state.map(s =>
+        s.userId === action.userId ? {...s, likes: s.likes + 1} : s
+      )
     default:
       return state
   }
