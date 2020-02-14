30f2b9adb69957bdb849cb6ff94a143140a5cc65
Ian Roberts <irob466@gmail.com>
2020-02-13 22:16:25 -0600

    preliminary socket logic added

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,9 +1,35 @@
+const uuid = require('uuid/v4')
 module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
+    })
+
+    // HOST ACTIONS
+
+    socket.on('create', () => {
+      // host creates game -->
+      // generates a game id and sends back to host
+    })
+
+    socket.on('start', gameId => {
+      // host starts game
+    })
+
+    // PLAYER ACTIONS
+
+    socket.on('join', gameId => {
+      // player joins room
+    })
+
+    socket.on('response', response => {
+      // player responds to prompt
+    })
+
+    socket.on('like', (userId, round) => {
+      // player likes a prompt
     })
   })
 }
